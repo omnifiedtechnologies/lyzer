2411ff0caecfb39dbd23a34644180f12860fc28d
Ben <ben@july7.pw>
2018-04-15 14:26:35 -0400

    update all file paths to relative for web server for embedable purposes

--- test/test.js
+++ test/test.js
@@ -1,8 +1,5 @@
 const Analytics = require('../index');
-const v8 = require('v8');
 
 let analytics = new Analytics();
-
-const func = () => console.log(analytics.getInMegabytes(analytics.getHeapUsed()) +  ' / ' +  analytics.getInMegabytes(analytics.getHeapTotal()) + " MB");
 
 analytics.startServer(8080);
