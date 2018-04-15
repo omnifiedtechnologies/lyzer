19c3c99c66d1d8f7c763d079b317bd7817f6c21a
Ben <ben@july7.pw>
2018-04-15 11:59:14 -0400

    add first implementation of chart.js code and REST communication

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,51 @@
 /*pointer to the canvas*/
 var ctx = document.getElementById("mainChart");
+
+var infos = [];
+var curLabel = 0;
+
+// HTTP get request
+const httpGET = (callback, url) => {
+    let http = new XMLHttpRequest();
+    http.onreadystatechange = () => {
+        if(http.readyState === 4 && http.status === 200){
+            callback(http.responseText);
+        }
+    }
+    http.open("GET", url);
+    http.send(null);
+};
+
+const addToChart = (chart, data) => {
+    chart.data.datasets[0].data.push(data);
+    curLabel++;
+    chart.data.labels[curLabel] = curLabel * 30 + " sec";
+    chart.update();
+}
+
+const update = () => {
+    httpGET((res) => {
+        let y = JSON.parse(res).used;
+        addToChart(chart, y);
+    }, '/getHeapInfo');
+}
+
+
+// every 30 seconds add this to the list of heap information
+update();
+setInterval(update, 30000);
+
+var chart = new Chart(ctx, {
+    type: 'line',
+    responsive: true,
+    maintainAspectRatio: false,
+    data: {
+        labels: [],
+        datasets: [
+            {
+                label: 'Heap usage (mb)',
+                data: infos
+            }
+        ]
+    }
+});
